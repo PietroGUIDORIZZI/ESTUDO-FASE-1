9fe5b3cf42444c470628e313dac0670acb76f064
PietroBin <Peterguidorizzi@gmail.com>
2026-06-26 02:09:16 -0300

    feat: terceiro challenge do dia.

---
+++ src/Desafio_Dia2_3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Desafio_Dia2_3 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type the first variable: ");
+        int var1 = Integer.parseInt(sc.nextLine());
+        System.out.println("Type the second variable: ");
+        int var2 = Integer.parseInt(sc.nextLine());
+
+
+        System.out.println("Before:");
+        System.out.println(var1);
+        System.out.println(var2);
+
+        int temp = var1;
+        var1 = var2;
+        var2 = temp;
+
+        System.out.println("After:");
+        System.out.println(var1);
+        System.out.println(var2);
+
+
+        sc.close();
+    }
+}
